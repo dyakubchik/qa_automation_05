812cee840cefc076f05d8986d2dc79b7c81e6f1d
dyakubchik <dmitry.yakubchik@gmail.com>
2022-09-10 16:21:10 -0400

    class_work (youtube) 9/10/2022

--- src/class_work/class_work_1_1.java
+++ src/class_work/class_work_1_1.java
@@ -6,19 +6,19 @@ public class class_work_1_1 {
 
     public static void main(String[] args) {
 
-//        int age = 7;
-//        int b = 5;
-//
-//        if(age == 7) {
-//            System.out.println("baby");
-//        }
-//        if(age == 7 && age <= 14){
-//            System.out.println("No baby");
-//        }
-//        else{
-//            //if(age == 8 && <= 14){
-//            System.out.println("Old");
-//        }
+        int age = 7;
+        int b = 5;
+
+        if(age == 7) {
+            System.out.println("baby");
+        }
+        if(age == 7 && age <= 14){
+            System.out.println("No baby");
+        }
+        else{
+            //if(age == 8 && <= 14){
+            System.out.println("Old");
+        }
 
         System.out.println("Add integer:");
         Scanner sc = new Scanner(System.in);
